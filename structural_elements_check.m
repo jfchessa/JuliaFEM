1edb506cb20022a100e8a9061fe657a1aab69e69
Jack Chessa <jfchessa@utep.edu>
2025-01-16 06:27:30 -0700

    more fixes

--- structural_elements_check.m
+++ structural_elements_check.m
@@ -37,9 +37,35 @@ ke = kmat_quad4(coord, E, nu, thk)
 % QUAD8
 coord = [0.0 0.0; 2.0 0.2; 2.1 0.9; 0.3 1.0;
          1.0 .1; 2.05 0.55; 1.2 1.0; 0.17 0.5 ];
-   
-coord = [0.0 0.0; 2.0 0.0; 2.0 2.0; 0.0 2.0; 1.0 0.0; 2.0 1.0; 1.0 2.0; 0.0 1.0 ];
 [be, j] = bmat_quad8(coord, xi)
-%ke = kmat_quad8(coord, E, nu, thk)
+ke = kmat_quad8(coord, E, nu, thk)
 
-dN = dshape_quad8(xi)
+% TETRA4
+coord = [0.0 0.0 0.0; 2.0 0.1 0.1; 0.2 1.0 0.0; 0.0 0.1 1.0];
+[be, V] = bmat_tetra4(coord);
+j = 6*V
+ke = kmat_tetra4(coord, E, nu);
+
+% TETRA10
+coord = [  0    1.0000         0         0    0.5000    0.5000         0         0    0.5000    0.0;     
+           0         0    1.0000         0         0    0.5000    0.5000         0         0    0.5000;
+           0         0         0    1.0000         0         0         0    0.5000    0.5000    0.5000 ]' ;
+xi = [.09, .1, .2];
+[be, j] = bmat_tetra10(coord, xi);
+bbe = be([1,2,3,6,4,5],:);
+ke = kmat_tetra10(coord, E, nu);
+
+% HEXA8
+coord = [0 0 0;2 0 0; 2 2 0; 0 2 0; 0 0 2; 2 0 2; 2 2 2; 0 2 2];
+xi = [.09, .1, .2];
+[be, j] = bmat_hexa8(coord, xi);
+bbe = be([1,2,3,6,4,5],:);
+ke = kmat_hexa8(coord, E, nu);
+
+% HEXA20
+coord = [0 0 0; 2 0 0; 2 2 0; 0 2 0; 0 0 2; 2 0 2; 2 2 2; 0 2 2;
+         0 1 0; 2 1 0; 1 2 0; 0 1 0; 0 1 2; 2 1 2; 1 2 2; 0 1 2;
+         0 0 1; 2 0 1; 2 2 1; 0 2 1];
+[be, j] = bmat_hexa20(coord, xi);
+bbe = be([1,2,3,6,4,5],:);
+ke = kmat_hexa20(coord, E, nu);
